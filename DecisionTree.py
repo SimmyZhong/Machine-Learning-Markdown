a00a4b31da9fd7d3b0774d6b83f7be31d6bd7897
SimmyZhong <30455031+SimmyZhong@users.noreply.github.com>
2018-03-28 21:58:33 +0800

    Update DecisionTree.py
    构建决策树

--- DecisionTree.py
+++ DecisionTree.py
@@ -25,10 +25,9 @@ class DecisionTree(object):
             shannonEnt -= prob * log(prob, 2)
         return shannonEnt
     
-    def classifyDataSet(self, axis, value):
+    def classifyDataSet(self, data_set, axis, value):
         """划分数据集"""
         
-        data_set = self.sample_data
         result_data = []
         for data in data_set:
             if data[axis] == value:
@@ -37,10 +36,9 @@ class DecisionTree(object):
                 result_data.append(result)
         return result_data
     
-    def getBestFeatureToSplit(self):
+    def getBestFeatureToSplit(self, sample_data):
         """根据香农熵选择最好的数据划分方式"""
         
-        sample_data = self.sample_data
         shannoEnt_base = self.calcShannonEnt(sample_data) # 计算原始数据集的香农熵
         labels = self.labels
         num_features = len(sample_data[0]) - 1
@@ -52,7 +50,7 @@ class DecisionTree(object):
             shannoEnt_new = 0.0 
             for value in unique_vals:        
                 # 计算每种划分方式的信息熵
-                result_data = self.classifyDataSet(i, value) #按特征值划分
+                result_data = self.classifyDataSet(sample_data, i, value) #按特征值划分
                 prob = len(result_data) / float(len(sample_data)) # 计算子集的概率
                 shannoEnt_new += prob * self.calcShannonEnt(result_data) #计算香农熵
            
@@ -63,15 +61,41 @@ class DecisionTree(object):
     
     def majorityCnt(self, classList):
         """多数表决的方法定义最后的叶子节点"""
-        pass
+        
+        class_count = {}
+        for value in classList:
+            class_count[value] = 1 + class_count.get(value, 0)
+        order_class_count = sorted(class_count.items(), key=lambda x: x[1],reverse=True )
+        return order_class_count[0][0]
     
-                
+    def createTree(self, dataSet, labels):
+        """创建决策树"""
+        
+        class_list = [example[-1] for example in dataSet]
+        if class_list.count(class_list[0]) == len(class_list):
+            return class_list[0] # 类别完全相同，停止划分
+        if len(dataSet[0]) == 1:
+            return self.majorityCnt(dataSet) # 遍历完所有特征时，返回出现次数最多的，即用多数表决方法定义最后的叶子节点
+        best_feature = self.getBestFeatureToSplit(dataSet)
+        best_feature_label = labels[best_feature]
+        mytree = {best_feature_label: dict()}
+        del(labels[best_feature])
+        feat_values = [example[best_feature] for example in dataSet]
+        unique_vals = set(feat_values)
+        for value in unique_vals:
+            sub_labels = labels[:]
+            mytree[best_feature_label][value] = self.createTree(self.classifyDataSet(dataSet, best_feature, value), sub_labels)
+        return mytree
+        
+        
 if __name__ == '__main__':
     sample_data = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
     tree = DecisionTree(sample_data, labels)
 #     print(tree.calcShannonEnt(sample_data))
-#     print(tree.classifyDataSet(0, 0))
-#     print(tree.getBestFeatureToSplit())
+#     print(tree.classifyDataSet(sample_data, 0, 1))
+#     print(tree.getBestFeatureToSplit(sample_data))
+#     print(tree.majorityCnt([1, 1, 2, 3, 3, 3, 4, 4, 4, 4]))
+#     print(tree.createTree(sample_data, labels))
 
             
