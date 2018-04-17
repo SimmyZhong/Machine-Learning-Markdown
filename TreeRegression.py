7b6f878e5a7f503329b758f99c919930e884865f
Simmy <wpzxm2011@163.com>
2018-04-17 21:42:12 +0800

    构建最优特征和阈值算法

--- TreeRegression.py
+++ TreeRegression.py
@@ -29,23 +29,67 @@ class TreeRegression(object):
         rightDataSet = dataMatrix[nonzero(dataMatrix[:, feature] <= value)[0], :]
         return leftDataSet, rightDataSet
 
-    def chooseBestFeature(self, dataMatrix, leafFunc, errToler, ops=(1, 4)):
+    @staticmethod
+    def leafFunc(dataMatrix):
+        return np.mean(dataMatrix[:, -1])
+
+    @staticmethod
+    def varFunc():
+        pass
+
+    def chooseBestFeature(self, dataMatrix, leafFunc, varFunc, ops=(1, 4)):
         """
         选取最优划分特征
-        :param dataMatrix:
-        :param leafFunc:
-        :param errToler:
-        :param ops:
-        :return:
+        :param dataMatrix: 训练集
+        :param leafFunc: 叶节点方法
+        :param errToler: 计算方差的引用
+        :param ops: 自定义参数
+        :return: 最优特征，最优特征阈值
         """
-        pass
+        # 统计剩余特征值的数目，若唯一则返回叶节点
+        if len(set(dataMatrix[:, -1].T.tolist()[0])) == 1:
+            return None, self.leafFunc(dataMatrix)
+        m, n = shape(dataMatrix)
+
+        # 计算初始总方差
+        varDataMatrix = np.var(dataMatrix[:, -1]) * shape(dataMatrix)[0]
+
+        # 初始化
+        bestS = np.inf
+        bestFeature = 0
+        bestVal = 0
+
+        # 遍历特征，以及特征值
+        for feature in range(n - 1):
+            for val in set(dataMatrix[:, feature].T.tolist()[0]):
+
+                # 划分左右子树
+                leftDataSet, rightDataSet = self.splitDataSet(dataMatrix, feature, val)
+
+                # 若 左右子树小于自定义参数，则不划分
+                if len(leftDataSet) < ops[1] or len(rightDataSet) < ops[1]:
+                    continue
+                varleftDataSet = np.var(leftDataSet[:, -1]) * shape(leftDataSet)[0]
+                varRightDataSet = np.var(rightDataSet[:, -1]) * shape(rightDataSet)[0]
+                sumS = varleftDataSet + varRightDataSet
+
+                # 若划分后左右子树方差和更小，则保存划分阈值和特征为最优
+                if bestS > sumS:
+                    bestS = sumS
+                    bestFeature = feature
+                    bestVal = val
+
+        # 划分后最优方差和小于自定义参数，则不进行划分，返回叶节点，否则返回最优划分特征和特征阈值
+        if bestS - varDataMatrix < ops[0]:
+            return None, self.leafFunc(dataMatrix)
+        return bestFeature, bestVal
 
     def createTreeRegression(self, dataMatrix, leafFunc, varFunc, ops=(1, 4)):
         """
         创建树回归
         :param dataMatrix: 训练集
         :param leafFunc: 创建叶节点方法
-        :param errToler: 方差计算方法
+        :param varFunc: 方差计算方法
         :param ops: 自定义参数组
         :return: 树回归
         """
@@ -54,15 +98,8 @@ class TreeRegression(object):
             return value
         treeRegress = dict()
         leftDataSet, rightDataSet = self.splitDataSet(dataMatrix, feature, value)
-        if len(leftDataSet) < ops[1] or len(rightDataSet) < ops[1]:
-            return value
-        varDataMatrix = np.power(np.var(dataMatrix[:, feature]), 2) * shape(dataMatrix)[0]
-        varleftDataSet = np.power(np.var(leftDataSet[:, feature]), 2)* shape(leftDataSet)[0]
-        varRightDataSet = np.power(np.var(rightDataSet[:, feature]), 2)* shape(rightDataSet)[0]
-        if varDataMatrix - varRightDataSet - varleftDataSet < ops[0]:
-            return value
-        treeRegress['left'] = leftDataSet
-        treeRegress['right'] = rightDataSet
+        treeRegress['left'] = self.createTreeRegression(leftDataSet, leafFunc, varFunc, ops=(1, 4))
+        treeRegress['right'] = self.createTreeRegression(rightDataSet, leafFunc, varFunc, ops=(1, 4))
         treeRegress['feature'] = feature
         treeRegress['val'] = value
         return treeRegress
