661a6c8f1fefc02eb410984d4be0571e76fa1a22
Simmy <wpzxm2011@163.com>
2018-04-19 22:55:51 +0800

    构建算法和debug

--- K-mean.py
+++ K-mean.py
@@ -26,6 +26,7 @@ def errFunc(VectorA, VectorB):
 
     return np.sqrt(np.sum(np.power(VectorA - VectorB, 2)))
 
+
 def createCenterField(dataSet, k):
     """构建k个质心集合"""
 
@@ -35,6 +36,37 @@ def createCenterField(dataSet, k):
         iRange = np.max(dataSet[:, i]) - np.min(dataSet[:, i])
         centerFiled[:, i] = np.min(dataSet[:, i]) + iRange * mat(np.random.rand(k, 1))
     return centerFiled
+
+
+def createKMeanClustering(dataSet, distanceFunc, k):
+    """
+    K均值聚类构建
+    :param dataSet: 数据集
+    :param distanceFunc: 距离方法
+    :param k: 质心数
+    :return: 质心集合， 方差
+    """
+
+    m, n = shape(dataSet)
+    centroids = createCenterField(dataSet, k)
+    clusterAssment = mat(zeros((m, 2)))
+    changeFlag = True
+    while changeFlag:
+        changeFlag = False
+        for i in range(m):
+            minDis, bestIndex, bestErr = np.inf, 0, 0
+            for j in range(k):
+                distance = distanceFunc(dataSet[i, :], centroids[j, :])
+                if distance < minDis:
+                    minDis = distance
+                    bestIndex = j
+            if clusterAssment[i, :] != bestIndex:
+                clusterAssment = True
+            clusterAssment[i, 0],  clusterAssment[i, 0] = bestIndex, minDis ** 2
+        for i in range(k):
+            centroids[i, :] = meanFunc(dataSet[np.nonzero(clusterAssment[:, 0].A == i)[0]])
+        return centroids, clusterAssment
+
 
 
 if __name__ == "__main__":
