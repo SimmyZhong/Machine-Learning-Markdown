4f2a4f32fbf22ece8984082bc3e45f0a2da4dfc0
Simmy-ali <wpzxm2011@163.com>
2018-05-07 22:11:44 +0800

    注释

--- PCA.py
+++ PCA.py
@@ -13,14 +13,21 @@ def readFromTxt(fileName):
     return np.mat(results)
 
 
-def pca(dataSet, topNfeat=6):
+def pca(dataSet, topNfeat=9999):
     """PCA算法降维"""
+    # 求均值
     meanVals = np.mean(dataSet, axis=0)
+    # 数据集减去均值
     meanRemoved = dataSet - meanVals
+    # 求协方差矩阵
     covmat = np.cov(dataSet, rowvar=0)
+    # 特征值和特征向量
     eigVals, eigVects = np.linalg.eig(np.mat(covmat))
+    # 将特征值按大小排序，取出前topNfeat个特征值
     eigValsSorted = np.argsort(eigVals)[: -(topNfeat+1):-1]
+    # 特征向量截取
     redEigVects = eigVects[:, eigValsSorted]
+    # 将数据集映射到低维空间
     lowDdataMat = meanRemoved * redEigVects
     reconMat = (lowDdataMat * redEigVects.T) + meanVals
     return lowDdataMat, reconMat
